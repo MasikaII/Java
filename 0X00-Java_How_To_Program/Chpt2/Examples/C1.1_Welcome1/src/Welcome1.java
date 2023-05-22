038f0854f1bdb65279231317a9bfb024281e04ea
Stephy <atienostephanie040@gmail.com>
2023-05-22 15:35:03 +0300

    a simple java application for text-printing

---
+++ 0X00-Java_How_To_Program/Chpt2/Examples/C1.1_Welcome1/src/Welcome1.java
@@ -0,0 +1,10 @@
+// Text-printing program
+
+public class Welcome1
+{
+    public static void main(String[] args)
+    {
+        // main method begins execution of Java application
+        System.out.println("Welcome to Java Programming!");
+    } // end method main
+} // end class Welcome1
