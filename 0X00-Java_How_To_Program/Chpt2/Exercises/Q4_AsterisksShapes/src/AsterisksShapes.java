d80a985881f14194c989373211105532b1bdf876
Stephy <atienostephanie040@gmail.com>
2023-06-01 14:39:04 +0300

    a program that displays shapes using asterisks

---
+++ 0X00-Java_How_To_Program/Chpt2/Exercises/Q4_AsterisksShapes/src/AsterisksShapes.java
@@ -0,0 +1,18 @@
+// A program that displays shapes using asterisks
+
+public class AsterisksShapes
+{
+    // method main begins execution of Java application
+    public static void main(String[] args)
+    {
+        System.out.println("*********    ***      *      *");
+        System.out.println("*       *  *     *   ***    * *");
+        System.out.println("*       * *       * *****  *   *");
+        System.out.println("*       * *       *   *   *     *");
+        System.out.println("*       * *       *   *  *       *");
+        System.out.println("*       * *       *   *   *     *");
+        System.out.println("*       * *       *   *    *   *");
+        System.out.println("*       *  *     *    *     * *");
+        System.out.println("*********    ***      *      *");
+    }
+}
