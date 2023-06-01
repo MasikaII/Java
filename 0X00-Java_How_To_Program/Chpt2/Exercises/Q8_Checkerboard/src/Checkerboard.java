d0b99e2404d54fc3ce1c73814e5732bbe2eea977
Stephy <atienostephanie040@gmail.com>
2023-06-01 14:58:48 +0300

    a program that displays a checkerboard pattern using asterisks

---
+++ 0X00-Java_How_To_Program/Chpt2/Exercises/Q8_Checkerboard/src/Checkerboard.java
@@ -0,0 +1,16 @@
+// An application that displays a checkerboard pattern using asterisks
+public class Checkerboard
+{
+    // main method begins execution of Java application
+    public static void main(String[] args)
+    {
+        System.out.println("* * * * * * * *");
+        System.out.println(" * * * * * * * *");
+        System.out.println("* * * * * * * *");
+        System.out.println(" * * * * * * * *");
+        System.out.println("* * * * * * * *");
+        System.out.println(" * * * * * * * *");
+        System.out.println("* * * * * * * *");
+        System.out.println(" * * * * * * * *");
+    }
+}
