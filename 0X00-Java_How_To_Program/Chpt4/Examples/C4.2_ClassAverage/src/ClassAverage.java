2661afbd2156a11afe414109a5f01d7a5cc9817e
Stephy <atienostephanie040@gmail.com>
2023-09-27 16:42:13 +0300

    solving the class-average problem using counter-controlled repetition

---
+++ 0X00-Java_How_To_Program/Chpt4/Examples/C4.2_ClassAverage/src/ClassAverage.java
@@ -0,0 +1,31 @@
+//Solving the class average problem using counter-controlled repetition
+import java.util.Scanner; //program uses class Scanner
+
+public class ClassAverage
+{
+    public static void main(String[] args)
+    {
+        //create Scanner to obtain input from command window
+        Scanner input = new Scanner(System.in);
+
+        //initialization phase
+        int total = 0; //initialize sum of grades
+        int gradeCounter = 1; //initialize grade to be entered next
+
+        //processing phase uses counter-controlled repetition
+        while(gradeCounter <= 10)
+        {
+            System.out.print("Enter grade: "); //prompt
+            int grade = input.nextInt(); //input next grade
+            total = total + grade; //add grade to total
+            gradeCounter = gradeCounter + 1; //increment counter by 1
+        }
+
+        //termination phase
+        int average = total / 10; //integer division yields integer result
+
+        //display total and average of grades
+        System.out.printf("%nTotal of all 10 grades is %d%n", total);
+        System.out.printf("Class average is %d%n", average);
+    }
+}//end class ClassAverage
